d922f571a4ff82de7eb9186088bea57b0e6011b1
Kingcodes95 <Ronniecodes95@gmail.com>
2025-04-17 17:43:15 -0500

    Reseting to a new DB

--- backend/app/database.py
+++
@@ -1,7 +0,0 @@
-# Initializes the Database so other parts of the app
-# can simply import the client from this module
-
-from supabase import create_client, Client
-from .config import SUPABASE_URL, SUPABASE_ANON_KEY
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
--- frontend/src/components/Login/LoginUI.jsx
+++ frontend/src/components/Login/LoginUI.jsx
@@ -1,51 +1,7 @@
 import './LoginUI.css';
-import { useState } from 'react';
-import { createCookie, isCookie, useNavigate } from 'react-router-dom';
 
 function Login() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
-    const [loading, setLoading] = useState(false);
-
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        setLoading(true);
-
-        try {
-            const response = await fetch("http://localhost:8000/auth/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    username,
-                    password,
-                }),
-            });
-
-            const result = await response.json();
-
-            if (!response.ok) {
-                alert(result.detail || "Login failed");
-                setLoading(false);
-                return;
-            }
-
-            localStorage.setItem("supabase_token", result.token);
-            localStorage.setItem("refresh_token", result.refresh_token);
-            localStorage.setItem("user", JSON.stringify(result.user));
-
-            console.log("Login success, Loading..");
-            navigate("/data");
-
-        } catch (err) {
-            console.error("Login error:", err);
-        } finally {
-            setLoading(false);
-        }
-    };
-
+    
     return(
         <>
             <form className="login-container" onSubmit={handleLogin}>
