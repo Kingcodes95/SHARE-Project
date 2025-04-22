d9f0724fb5ac33b62733959178346a3a042e7342
Kingcodes95 <Ronniecodes95@gmail.com>
2025-04-22 17:25:13 -0500

    Made get_current_user with the same logic as /data router in order to use it other places

---
+++ backend/app/supabase_client/depends.py
@@ -0,0 +1,15 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from .supabase_client import supabase
+
+auth_bearer = HTTPBearer()
+
+async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(auth_bearer)):
+    access_token = credentials.credentials
+    supabase.auth.set_session(access_token, '')
+    user_response = supabase.auth.get_user()
+
+    if not user_response or not user_response.user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid or expired token')
+
+    return user_response.user
